37ff8bcd9693028d23d865895517ce23e0edeec0
Julius Suominen <julius.suominen@gmail.com>
2017-04-18 20:55:51 +0300

    06

--- 06_another_cell.js
+++ 06_another_cell.js
@@ -1,4 +1,4 @@
-/*Another cell
+/* Another cell
 
 Implement a cell type named StretchCell(inner, width, height) that conforms to the
 table cell interface described earlier in the chapter.
@@ -7,9 +7,50 @@ It should wrap another cell (like UnderlinedCell does) and ensure that the resul
 has at least the given width and height, even if the inner cell would naturally be smaller.
 */
 
+/* This all is here because we're not using EJS code sandbox */
+function repeat(string, times) {
+  var result = '';
+  for (var i = 0; i < times; i++)
+    result += string;
+  return result;
+}
+function TextCell(text) {
+  this.text = text.split("\n");
+}
+TextCell.prototype.minWidth = function() {
+  return this.text.reduce(function(width, line) {
+    return Math.max(width, line.length);
+  }, 0);
+};
+TextCell.prototype.minHeight = function() {
+  return this.text.length;
+};
+TextCell.prototype.draw = function(width, height) {
+  var result = [];
+  for (var i = 0; i < height; i++) {
+    var line = this.text[i] || "";
+    result.push(line + repeat(" ", width - line.length));
+  }
+  return result;
+};
+/* Solution starts here */
 
+function StretchCell(inner, width, height) {
+  this.inner = inner;
+  this.width = width;
+  this.height = height;
+}
+StretchCell.prototype.minWidth = function () {
+  return Math.max(this.width, this.inner.minWidth());
+};
+StretchCell.prototype.minHeight = function () {
+  return Math.max(this.height, this.inner.minHeight());
+};
+StretchCell.prototype.draw = function (width, height) {
+  return this.inner.draw(width, height);
+};
 
-var sc = new StretchCell(new TextCell("abc"), 1, 2);
+const sc = new StretchCell(new TextCell('abc'), 1, 2);
 console.log(sc.minWidth());
 // → 3
 console.log(sc.minHeight());
